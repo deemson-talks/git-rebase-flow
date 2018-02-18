147573fec198ebeddeb64e9239010705a7ecd0c3
deemson <deemson.k@gmail.com>
2018-02-18 10:00:15 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './index.pug'
+import * as Reveal from 'reveal.js'
+import 'reveal.js/css/reveal.css'
+import 'reveal.js/css/theme/black.css'
+import * as hljs from 'highlight.js'
+import 'highlight.js/styles/gruvbox-dark.css'
+
+
+Reveal.initialize({
+});
+hljs.initHighlightingOnLoad();
